3efa2165c7f8b3785b0129a6b5e551e9f70399a5
qofw <haliban256@gmail.com>
2020-04-13 12:51:36 +0600

    cf633 A brute force

---
+++ codeforces codes/cf633_A.cpp
@@ -0,0 +1,27 @@
+/*
+contest :
+codeforces 633 (div 2)
+
+problem link :
+https://codeforces.com/contest/1339/problem/A
+
+tag :
+brute force
+
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long int tc,a,b;
+    cin>>tc;
+    for(a=0;a<tc;a++)
+    {
+        cin>>b;
+        cout<<b<<endl;
+    }
+    return 0;
+}
