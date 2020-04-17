371ee3ed9eeee2fae92322f648ae7483f309d247
qofw <haliban256@gmail.com>
2020-04-17 21:32:09 +0600

    cf 634 div 2 A math

---
+++ codeforces codes/cf635_A.cpp
@@ -0,0 +1,26 @@
+/*
+contest :
+codeforces 635 (div 2)
+
+problem link :
+https://codeforces.com/contest/1337/problem/A
+
+tag :
+math
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int tc,a,b,c,d,n,p,q,r,s;
+    cin>>tc;
+    for(a=0;a<tc;a++)
+    {
+      cin>>p>>q>>r>>s;
+      cout<<q<<" "<<r<<" "<<r<<endl;
+    }
+    return 0;
+}
+
